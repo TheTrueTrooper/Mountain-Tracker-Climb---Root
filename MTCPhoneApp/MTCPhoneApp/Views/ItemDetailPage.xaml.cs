e85befd403bf8ab39e37f79a2e60d274b5a3d777
Angelo <Soviet_Trooper@live.ca>
2021-01-27 12:51:59 -0800

    Added some boiler plate phone app code.
    -Added some boiler plate phone app code
    -Upgraded project to VS2019P3
    -Trouble shot alot to get the phone emulator working as defualts settings were in fact broken (least with my machines build); Issues were (Since it was OS related stuff):
    1. No hardware acceleration was installed at first intalled -> installed Intel Hardware Accerated Execution  Manager (HAXM)
    2. My VS 2017 Enterpise didnt have the newer Android in the install list (think it would just retriv all of them and not be hard code but ok MS) -> installed a full VS2019P3 Community and downsized Enterpries 2017.
    3.(hard one) The Emulator wouldnt start fully. (some issue with the default GPU setting) ->Changed GPU to 'Mesa' from 'Host'.
    4. Also just generally broke it once -> discarded changes once after updating the Xam.Forms and breaking it,

---
+++ MTCPhoneApp/MTCPhoneApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MTCPhoneApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MTCPhoneApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ MTCPhoneApp/MTCPhoneApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MTCPhoneApp.Models;
+using MTCPhoneApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MTCPhoneApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
